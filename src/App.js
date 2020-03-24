a1241bfda1275ff05a3f39c2b977f892a26b8a6f
Watcharapong Nanthasan <watcharapong@blueparking.co.th>
2020-03-24 21:37:02 +0700

    tools

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -12,15 +8,15 @@ const App = () => {
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
-      breed: "Havanese"
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
-      breed: "Cockatiel"
+      breed: "Cockatiel",
     }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
