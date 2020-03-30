2a81978c4ad7539e6293686cd056d798b9f5556a
Watcharapong Nanthasan <watcharapong@blueparking.co.th>
2020-03-30 14:29:42 +0700

    Error Boundaries

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
-import Carousel from './Carousel'
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   state = { loading: true };
@@ -28,7 +29,7 @@ class Details extends React.Component {
 
     return (
       <div className="details">
-        <Carousel media={media}/>
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
@@ -40,4 +41,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
